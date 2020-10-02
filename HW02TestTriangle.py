abecf74f88dff4bf8f1766b4596acc3a2bce4bf8
fs412 <49499662+fs412@users.noreply.github.com>
2020-10-02 07:10:32 -0400

    Add files via upload

---
+++ HW02TestTriangle.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Updated Jan 21, 2018
+The primary goal of this file is to demonstrate a simple unittest implementation
+
+@author: jrr
+@author: rk
+"""
+
+import unittest
+
+from HW02Triangle import classifyTriangle
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestTriangles(unittest.TestCase):
+    # define multiple sets of tests as functions with names that begin
+
+    def testRightTriangleA(self): 
+        self.assertEqual(classifyTriangle(3,4,5),'Scalene Right','3,4,5 is a Scalene Right triangle')
+
+    def testRightTriangleB(self): 
+        self.assertEqual(classifyTriangle(5,3,4),'Scalene Right','5,3,4 is a Scalene Right triangle')
+        
+    def testEquilateralTriangles(self): 
+        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testIsoscelesTriangles(self): 
+        self.assertEqual(classifyTriangle(5,5,6),'Isosceles','5,5,6 should be isosceles')
+
+    def testInvalidInput(self): 
+        self.assertEqual(classifyTriangle(-1,1,1),'InvalidInput')
+        self.assertEqual(classifyTriangle(201,201,201),'InvalidInput')
+
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(3,3,9),'NotATriangle')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
+
